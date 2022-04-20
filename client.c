0965b28bf1e111da7752f3f1eab5c3e49d2915b0
eduardordev <eduardorh1312>
2022-04-20 08:49:18 -0400

    handle error in client

--- client.c
+++ client.c
@@ -20,6 +20,9 @@
 #define MAXLINE 4096
 #define SA struct sockaddr
 
+void handle_error(const char *fmt, ...);
+
+
 int main(int argc, char **argv)
 {
     int sockfd, n;
@@ -39,4 +42,27 @@ int main(int argc, char **argv)
     memset(recvline, 0, MAXLINE);
     exit(0);
     
+}
+
+void handle_error(const char *fmt, ...)
+{
+    int errno_save;
+    va_list ap;
+
+    errno_save = errno;
+
+    va_start(ap, fmt);
+    vfprintf(stdout, fmt, ap);
+    fprintf(stdout, "\n");
+    fflush(stdout);
+
+    if (errno_save != 0) {
+        fprintf(stdout, "(errno = %d) : %s\n",errno_save,
+        strerror(errno_save));
+        fprintf(stdout, "\n");
+        fflush(stdout);
+    }
+    va_end(ap);
+
+    exit(1);
 }
